f1a2839184eb793ec475774c86f2571ddbe43b59
Chris Seaman <chris.seaman@burnet.edu.au>
2026-01-30 12:19:11 +1100

    clear main

--- hbv_vimc/__init__.py
+++
@@ -1,9 +0,0 @@
-import atomica as at
-
-root = (at.parent_dir()/'..').resolve()
-
-from .constants import *
-from .utils import *
-from .plotting import *
-from .scenarios import *
-from .calib_diags import *
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name="hbv-vimc",
-    packages=find_packages(),
-    install_requires=[
-    ],
-)
