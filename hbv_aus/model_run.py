49a1f7aa385bd4e9cb607ff2e21d630ad1e741a1
Chris Seaman <chris.seaman@burnet.edu.au>
2026-02-26 11:44:54 +1100

    add pop YAML calibrate

--- hbv_aus/model_run.py
+++ hbv_aus/model_run.py
@@ -1,20 +1,32 @@
 import atomica as at
 import numpy as np
-from hbv_aus.utils import get_github_folder,_get_sharepoint_folder
+from hbv_aus.utils import _get_github_folder,_get_sharepoint_folder
 
 def calibrate_model():
     # TODO: Update to include ABC approach once ready
     # TODO: Allow external definition of FW and DB to be used
-    # TODO: Add functionality (script called utils_model) to assess calibration fits
 
     #Set up Atomica project from latest update
-    F = at.ProjectFramework(get_github_folder()+f"framework/fw_popsizes.xlsx")
+    F = at.ProjectFramework(_get_github_folder()+f"framework/fw_popsizes.xlsx")
     P = at.Project(framework=F, databook=_get_github_folder() + f"databook/db_demographics.xlsx",
                                 do_run = False, sim_start = 1980, sim_end = 2071, sim_dt = 1)
     cal = P.parsets[0].copy()
-    cal = P.calibrate(max_time=300, parset=cal, adjustables=["acm_rate"],
-                      measurables=["aus_pop"],
-                      default_min_scale=0.5, default_max_scale=2)
+
+    # Population Calibrations
+    cal = P.calibrate(parset = cal, yaml = _get_github_folder()+f"calibrations/YAML/calibrate_populations.yaml")
+    # TODO: Disease/Prevalence Calibrations
+    # TODO: Care Calibrations
+
+    # Save Calibration to GitHub
+    cal.save_calibration(_get_github_folder()+f"calibrations/Y-factors/calibrate_populations.xlsx")
+
+
+
+
+
+
+
+
 
 
 def extract_data()
